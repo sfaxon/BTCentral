a58c5417d9980be47e24c5e1ca19a56ef04a2de4
Seth Faxon <seth.faxon@gmail.com>
2014-10-01 19:19:53 -0700

    adding basic classes

---
+++ BTCentral/Models/Peripheral.swift
@@ -0,0 +1,13 @@
+//
+//  Peripheral.swift
+//  BTCentral
+//
+//  Created by Seth Faxon on 10/1/14.
+//  Copyright (c) 2014 Seth Faxon. All rights reserved.
+//
+
+import UIKit
+
+class Peripheral: NSObject {
+   
+}
